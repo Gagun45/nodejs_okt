1ebdefe26ff733b7c815a4df343af337881911dd
Gagun45 <selyanchyn45@gmail.com>
2026-01-10 21:12:01 +0200

    comms added

--- index.js
+++ index.js
@@ -11,19 +11,23 @@ const logInfoByPath = async (targetPath) => {
 };
 
 const main = async () => {
+    //creating baseFolder
   const baseFolderPath = path.join(__dirname, "baseFolder");
   await fs.mkdir(baseFolderPath, {
     recursive: true,
   });
   await logInfoByPath(baseFolderPath);
 
+  //creating subfolders and files
   for (let i = 1; i <= FOLDERS_COUNT; i++) {
+    //creating subfolders
     const newDirPath = path.join(baseFolderPath, `folder${i}`);
     await fs.mkdir(newDirPath, {
       recursive: true,
     });
     await logInfoByPath(newDirPath);
 
+    //creating files
     const fileCreatePromises = Array.from({length: FILES_COUNT}, (_,j)=>{
         const newFilePath = path.join(newDirPath, `text${j+1}.txt`)
         return fs.writeFile(newFilePath, '').then(()=>logInfoByPath(newFilePath))
