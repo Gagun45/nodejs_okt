abfe5a47a0df6aa8d770dd482b8f18cc9af57117
Gagun45 <selyanchyn45@gmail.com>
2026-01-12 17:06:32 +0200

    husky removed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import express, { Request, Response } from "express";
 
-import { ParsedPath } from "path";
-
 import { getUserById, getUsers, resetDb, updateUsers } from "./dbActions";
 import {
     parseUserIdOrThrow,
