ab3ce55696b0e9625fc7176410694d1a7155ca0c
Gagun45 <selyanchyn45@gmail.com>
2026-01-12 21:40:52 +0200

    validation fixed

--- src/validation.ts
+++ src/validation.ts
@@ -1,11 +1,13 @@
 import { ApiError } from "./errors/api-error";
 
-export const validateNameOrThrow = (name: string) => {
-    if (name.length < 4) throw new ApiError("Name validation failed", 400);
+export const validateNameOrThrow = (name: any) => {
+    if (typeof name !== "string" || name.length < 4)
+        throw new ApiError("Name validation failed", 400);
 };
 
-export const validateAgeOrThrow = (age: number) => {
-    if (age < 0) throw new ApiError("Age validation failed", 400);
+export const validateAgeOrThrow = (age: any) => {
+    if (typeof age !== "number" || age < 0)
+        throw new ApiError("Age validation failed", 400);
 };
 
 export const parseUserIdOrThrow = (value: string) => {
