92c846f6a8f1aaae2df998d0011ab5a1a6facf0e
Gagun45 <selyanchyn45@gmail.com>
2026-01-14 19:15:46 +0200

    custom errors removed

--- src/errors/errors.ts
+++
@@ -1,5 +0,0 @@
-import { ApiError } from "./api-error";
-
-export const customErrors = {
-    notFound: (value: string) => new ApiError(`${value} not found`, 404),
-};
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,4 @@
-import { customErrors } from "../errors/errors";
+import { ApiError } from "../errors/api-error";
 import { userRepository } from "../repositories/user.repository";
 import { TokenPayloadType } from "../types/token.types";
 import { IUser, SingUpDtoType, UpdateUserDtoType } from "../types/user.types";
@@ -14,25 +14,25 @@ export const userService = {
     },
     getById: async (userId: string): Promise<IUser> => {
         const user = await userRepository.getById(userId);
-        if (!user) throw customErrors.notFound("User");
+        if (!user) throw new ApiError("User not found", 404);
         return user;
     },
     getMe: async (payload: TokenPayloadType): Promise<IUser> => {
         const user = await userRepository.getById(payload.userId);
-        if (!user) throw customErrors.notFound("User");
+        if (!user) throw new ApiError("User not found", 404);
         return user;
     },
     delete: async (userId: string): Promise<void> => {
         const deletedUser = await userRepository.delete(userId);
-        if (!deletedUser) throw customErrors.notFound("User");
+        if (!deletedUser) throw new ApiError("User not found", 404);
     },
     deleteMe: async (payload: TokenPayloadType): Promise<void> => {
         const deletedUser = await userRepository.delete(payload.userId);
-        if (!deletedUser) throw customErrors.notFound("User");
+        if (!deletedUser) throw new ApiError("User not found", 404);
     },
     update: async (userId: string, dto: UpdateUserDtoType): Promise<IUser> => {
         const user = await userRepository.update(userId, dto);
-        if (!user) throw customErrors.notFound("User");
+        if (!user) throw new ApiError("User not found", 404);
         return user;
     },
     updateMe: async (
@@ -40,7 +40,7 @@ export const userService = {
         dto: UpdateUserDtoType,
     ): Promise<IUser> => {
         const user = await userRepository.update(payload.userId, dto);
-        if (!user) throw customErrors.notFound("User");
+        if (!user) throw new ApiError("User not found", 404);
         return user;
     },
     reset: async (): Promise<void> => {
