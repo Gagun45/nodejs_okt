a978a5cb6387cb53aa3cd63ae3b974e16e17ef6a
Gagun45 <selyanchyn45@gmail.com>
2026-01-19 18:09:24 +0200

    crons job file renamed

--- src/crons/cron.runner.ts
+++ src/crons/cron.runner.ts
@@ -1,4 +1,4 @@
-import { cleanupExpriredRefreshTokensJob } from "./test.cron";
+import { cleanupExpriredRefreshTokensJob } from "./cleanup-exp-ref-toks.cron";
 
 export const cronRunner = () => {
     cleanupExpriredRefreshTokensJob.start();
